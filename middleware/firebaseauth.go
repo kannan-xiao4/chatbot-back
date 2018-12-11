b238bc24d4bbdb8c083fa86e1921ae858298b88c
Takashi Kannan <kannan.takashi@techcross.co.jp>
2018-12-11 11:29:00 +0900

    firebaseauthによるjwt認証処理を修正

--- middleware/firebaseauth.go
+++ middleware/firebaseauth.go
@@ -4,42 +4,61 @@ import (
 	"context"
 	"firebase.google.com/go"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"google.golang.org/api/option"
 	"log"
 	"net/http"
-	"os"
-	"strings"
 )
 
-func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// Firebase SDK のセットアップ
-		opt := option.WithCredentialsFile(os.Getenv("CREDENTIALS"))
-		app, err := firebase.NewApp(context.Background(), nil, opt)
-		if err != nil {
-			fmt.Printf("error: %v\n", err)
-			os.Exit(1)
-		}
-		auth, err := app.Auth(context.Background())
-		if err != nil {
-			fmt.Printf("error: %v\n", err)
-			os.Exit(1)
-		}
+func FirebaseAuth(c *gin.Context) {
 
-		// クライアントから送られてきた JWT 取得
-		authHeader := r.Header.Get("Authorization")
-		idToken := strings.Replace(authHeader, "Bearer ", "", 1)
-
-		// JWT の検証
-		token, err := auth.VerifyIDToken(context.Background(), idToken)
-		if err != nil {
-			// JWT が無効なら Handler に進まず別処理
-			fmt.Printf("error verifying ID token: %v\n", err)
-			w.WriteHeader(http.StatusUnauthorized)
-			w.Write([]byte("error verifying ID token\n"))
-			return
-		}
-		log.Printf("Verified ID token: %v\n", token)
-		next.ServeHTTP(w, r)
+	// Firebase SDK のセットアップ
+	opt := option.WithCredentialsFile("credentials/chatbot-base-firebase-adminsdk-b3c63-219633f486.json")
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		fmt.Printf("error initializing app: %v", err)
+		firebaseUninitialized(c)
+		return
 	}
+	auth, err := app.Auth(context.Background())
+	if err != nil {
+		fmt.Printf("error initializing app: %v", err)
+		firebaseUninitialized(c)
+		return
+	}
+
+	authorizationHeader := c.Request.Header.Get("Authorization")
+	if authorizationHeader == "" {
+		unauthorized(c)
+		return
+	}
+	if authorizationHeader[:7] != "Bearer " {
+		unauthorized(c)
+		return
+	}
+	tokenString := authorizationHeader[7:]
+
+	// JWT の検証
+	token, err := auth.VerifyIDToken(context.Background(), tokenString)
+	if err != nil {
+		// JWT が無効なら Handler に進まず別処理
+		fmt.Printf("error verifying ID token: %v\n", err)
+		unauthorized(c)
+		return
+	}
+	log.Printf("Verified ID token: %v\n", token)
+}
+
+func unauthorized(c *gin.Context) {
+	c.JSON(http.StatusUnauthorized, gin.H{
+		"error": http.StatusText(http.StatusUnauthorized),
+	})
+	c.Abort()
+}
+
+func firebaseUninitialized(c *gin.Context) {
+	c.JSON(http.StatusUnauthorized, gin.H{
+		"error": http.StatusText(http.StatusInternalServerError),
+	})
+	c.Abort()
 }
